c803436143bf220a8ee786d37f67e2efb4178eb9
Kate Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2019-10-30 14:57:51 -0500

    select page because a custom bundle page that prepops visually based on url params

--- assets/custom.jquery.js
+++ assets/custom.jquery.js
@@ -336,7 +336,21 @@ function deleteCookie(name) {
 if(window.location.href.indexOf("countdown") > -1) {
   localStorage.setItem("timer", true);
 }
+// url params as variables 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
 // redirect v. no redirect 
 $(document).ready(function(){
   var flowFinish = readCookie('flowFinish')
