df70edd51863a65e7172e1008af910a829932246
Kate Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2020-02-11 16:40:16 -0600

    updates the survey to be reason you purchased your gekks and where did you last buy shoes

--- assets/customcheckout.js
+++ assets/customcheckout.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   var surveyShown = readCookie('postpurchase');
-  if ($(".thank-you__additional-content").length && !surveyShown) {
+  // if ($(".thank-you__additional-content").length && !surveyShown) {
+      if ($(".thank-you__additional-content").length ) {
+
     $('.postpurchase').show().insertAfter('.thank-you__additional-content'); 
     createCookie('postpurchase', 'true'); 
   }
